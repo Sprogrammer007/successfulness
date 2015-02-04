a88e3bd918bf8085d86dbe8b0d46ea030eb0580a
Steve007 <steve007@Steve007s-Mac-Pro.local>
2015-02-04 11:50:44 -0500

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 20150105213312) do
     t.string   "encrypted_password",         default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.integer  "sign_in_count",              default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
@@ -55,9 +54,6 @@ ActiveRecord::Schema.define(version: 20150105213312) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",            default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
     t.integer  "group_id"
     t.string   "username",                   default: "", null: false
     t.string   "type"
@@ -75,7 +71,6 @@ ActiveRecord::Schema.define(version: 20150105213312) do
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
